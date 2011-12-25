dc9e7c6ed3a73bfd3be305ab804a9123ab143db7
Stephen Jennings <Stephen.G.Jennings@gmail.com>
2011-12-25 11:27:56 -0800

    Initial commit.

---
+++ Heap.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿//------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Stephen Jennings">
+//   Copyright 2011 Stephen Jennings. Licensed under the Apache License, Version 2.0.
+// </copyright>
+//------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("Heap")]
+[assembly: AssemblyCopyright("Copyright 2011 Stephen Jennings")]
+
+[assembly: AssemblyTitle("Heap.Web")]
+[assembly: AssemblyDescription("The ASP.NET project for the Heap knowledge management system.")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("0cdd1215-f720-4a8e-a891-0b5a82ad1692")]
---
+++ Heap.Web/Properties/VersionAssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿//------------------------------------------------------------------------------------
+// <copyright file="VersionAssemblyInfo.cs" company="Stephen Jennings">
+//   Copyright 2011 Stephen Jennings. Licensed under the Apache License, Version 2.0.
+// </copyright>
+//------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
